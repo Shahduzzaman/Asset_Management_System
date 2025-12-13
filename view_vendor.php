e0148f4daadae997cc912e310ab28122db516cf5
Shahduzzaman <mdshahduzzaman@gmail.com>
2025-12-13 13:12:38 +0600

    Update view_vendor.php

--- view_vendor.php
+++ view_vendor.php
@@ -98,9 +98,6 @@ $conn->close();
 <body class="bg-gray-100 min-h-screen">
 
     <div class="container mx-auto p-4 sm:p-6 lg:p-8">
-        <div class="flex justify-between items-center mb-6">
-            <h1 class="text-3xl font-bold text-gray-800">Vendor List</h1>
-        </div>
         
         <!-- Search Bar -->
         <div class="mb-6">
