1ac5d5c119458e23055f37fe4821438c846dfed2
Shahduzzaman <mdshahduzzaman@gmail.com>
2025-12-14 13:14:06 +0600

    Update add_branch.php

--- add_branch.php
+++ add_branch.php
@@ -1,17 +1,7 @@
 <?php
-session_start();
 
-// --- START: SESSION & SECURITY CHECKS ---
-$idleTimeout = 1800; // 30 minutes
-if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $idleTimeout)) {
-    session_unset(); session_destroy(); header("Location: index.php?reason=idle"); exit();
-}
-$_SESSION['last_activity'] = time();
-if (!isset($_SESSION["user_id"])) {
-    header("Location: index.php"); exit();
-}
+require_once 'session_guard.php';
 $current_user_id = $_SESSION['user_id'];
-// --- END: SESSION & SECURITY CHECKS ---
 
 require_once 'connection.php';
 
