bb0164b1add07adc705bc240e332b6258dbe7e51
Shahduzzaman <mdshahduzzaman@gmail.com>
2025-12-14 13:15:17 +0600

    Update stock_monitor.php

--- stock_monitor.php
+++ stock_monitor.php
@@ -1,21 +1,8 @@
 <?php
-session_start();
+require_once 'session_guard.php';
+
 require_once 'connection.php';
 
-// --- Session & Security Checks (match your style) ---
-$idleTimeout = 1800;
-if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $idleTimeout)) {
-    session_unset();
-    session_destroy();
-    header("Location: index.php?reason=idle");
-    exit();
-}
-$_SESSION['last_activity'] = time();
-
-if (!isset($_SESSION["user_id"])) {
-    header("Location: index.php");
-    exit();
-}
 
 // --- Helper utilities -----------------------------------------------------
 function to_int($v) { return (int)$v; }
