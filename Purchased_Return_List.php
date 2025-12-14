de8aa20740192be23b263dd565df07c5c34a1889
Shahduzzaman <mdshahduzzaman@gmail.com>
2025-12-14 13:15:04 +0600

    Update Purchased_Return_List.php

--- Purchased_Return_List.php
+++ Purchased_Return_List.php
@@ -1,5 +1,8 @@
 <?php
-session_start();
+require_once 'session_guard.php';
+
+$user_id = (int)$_SESSION['user_id'];
+
 require_once 'connection.php'; // expects $conn (mysqli)
 
 // --- helper ---
@@ -12,20 +15,6 @@ function is_admin() {
     return isset($_SESSION['role']) && (int)$_SESSION['role'] === 1;
 }
 
-// --- session / idle handling ---
-$idleTimeout = 1800;
-if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $idleTimeout)) {
-    session_unset();
-    session_destroy();
-    header("Location: index.php?reason=idle");
-    exit();
-}
-$_SESSION['last_activity'] = time();
-
-if (!isset($_SESSION['user_id'])) {
-    header("Location: index.php");
-    exit();
-}
 
 $user_id = (int)$_SESSION['user_id'];
 $user_role = isset($_SESSION['role']) ? (int)$_SESSION['role'] : 0;
