bdecbd348d2ce6c38cebe52236ba452b3914ca25
Shahduzzaman <mdshahduzzaman@gmail.com>
2026-01-26 00:04:45 +0600

    Update session_guard.php

--- session_guard.php
+++ session_guard.php
@@ -6,12 +6,10 @@ if (session_status() === PHP_SESSION_NONE) {
 $idleTimeout = 1800; // 30 minutes
 $sessionExpired = false;
 
-// Not logged in
 if (!isset($_SESSION['user_id'])) {
     $sessionExpired = true;
 }
 
-// Idle timeout
 if (
     isset($_SESSION['last_activity']) &&
     (time() - $_SESSION['last_activity'] > $idleTimeout)
@@ -23,7 +21,6 @@ if ($sessionExpired) {
     session_unset();
     session_destroy();
 
-    // Break out of iframe if needed
     if (isset($_GET['iframe'])) {
         echo '<script>window.top.location.href="index.php?reason=expired";</script>';
     } else {
@@ -32,5 +29,4 @@ if ($sessionExpired) {
     exit();
 }
 
-// Update activity timestamp
 $_SESSION['last_activity'] = time();
